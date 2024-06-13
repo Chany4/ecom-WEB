b3ff1cf030516370f4aeec4aaf2e5ec263430bc9
Chany4 <chandreleighdavids04052005@gmail.com>
2024-06-13 15:43:28 +0200

    added stuffs

---
+++ script/checkout.js
@@ -0,0 +1,115 @@
+let cart = [];
+
+const itemStored = localStorage.getItem('checkout');
+if (itemStored) {
+    try {
+        cart = JSON.parse(itemStored);
+    } catch (error) {
+        console.error('Error', error);
+    }
+}
+
+function displayProducts() {
+    let cartProductsContainer = document.querySelector('#cartProducts');
+    cartProductsContainer.innerHTML = '';
+    let table = document.createElement('table');
+    table.classList.add('table', 'table-bordered', 'table-responsive');
+
+    let thead = document.createElement('thead');
+    thead.innerHTML = `
+        <tr>
+            <td>Image</td>
+            <td>Item</td>
+            <td>Description</td>
+            <td>Color</td>
+            <td>Quantity</td>
+            <td>Price</td>
+            <td>Action</td>
+        </tr>
+    `;
+    table.appendChild(thead);
+
+    let aggregatedItems = {};
+
+    let totalPrice = 0;
+
+    cart.forEach((items, index) => {
+        let key = `${items.name} | ${items.category} | ${items.price}`;
+        if (aggregatedItems[key]) {
+            aggregatedItems[key].quantity += 1;
+        } else {
+            aggregatedItems[key] = { ...items, quantity: 1, index: index };
+        }
+    });
+
+    let tbody = document.createElement('tbody');
+
+    Object.keys(aggregatedItems).forEach(key => {
+        let items = aggregatedItems[key];
+        let row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td class="text-center"><img src="${items.image}" alt="image_of_product" style="width:150px"></td>
+            <td class="text-center pt-5">${items.name}</td>
+            <td class="text-center pt-5">${items.description}</td>
+            <td class="text-center pt-5">${items.color}</td>
+            <td class="text-center pt-5">${items.quantity}</td>
+            <td class="text-center pt-5">${items.price}</td>
+            <td class="text-center pt-5"><button type="button" class="delete_Item" onclick="deleteProduct(${items.index})">X</button></td>
+        `;
+
+        tbody.appendChild(row);
+        totalPrice += items.price * items.quantity;
+    });
+    table.appendChild(tbody);
+
+    cartProductsContainer.appendChild(table);
+
+    let totalPriceElement = document.createElement('div');
+    totalPriceElement.classList.add('mx-auto', 'font-weight-bold');
+    totalPriceElement.innerHTML = `Total Price $${totalPrice}`;
+    cartProductsContainer.appendChild(totalPriceElement);
+}
+
+function deleteProduct(index) {
+    cart.splice(index, 1);
+    localStorage.setItem('checkout', JSON.stringify(cart));
+    displayProducts();
+    console.log('Product deleted, Updated checkout items:', cart);
+}
+
+function purchase() {
+    if (cart.length === 0) {
+        console.error('Cannot proceed with purchase: Cart is empty.');
+        alert('Your cart is empty. Please add items before proceeding with the purchase.');
+        return;
+    }
+
+    const confirmPurchase = confirm('Are you sure you want to proceed with the purchase?');
+    if (!confirmPurchase) {
+        console.log('Purchase canceled.');
+        return;
+    }
+
+    try {
+        cart = [];
+        localStorage.removeItem('checkout');
+        displayProducts();
+        console.log('Your purchase has been succsessfull');
+        alert('Thank you for your purchase!');
+    } catch (error) {
+        console.error('Error processing purchase:', error);
+        alert('There was an error processing your purchase. Please try again later.');
+    }
+}
+
+const purchaseButton = document.getElementById('purchaseButton');
+purchaseButton.addEventListener('click', purchase);
+
+const alertbtn = document.querySelector('.purchase-button')
+
+// alertbtn.addEventListener('click', ()=> {
+//     alert('Your purchase has been successfull')
+// })
+
+displayProducts();
---
+++ script/product.js
@@ -0,0 +1,320 @@
+let products =  [
+    {
+        id: 1,
+        name: "Poison Ivy",
+        category: "leather",
+        color: "green",
+        description: "Christian Dior Micro Lady Dior Green Quilted Cannage Lambskin Cd Charm Mini Bag",
+        image: "https://chany4.github.io/ecomWEB/images/leather/Christian%20Dior%20Micro%20Lady%20Dior%20Green%20Quilted%20Cannage%20Lambskin%20Cd%20Charm%20Mini%20Bag.jpeg",
+        price: 1780,
+    },
+    {
+        id: 2,
+        name: "Butterflix",
+        category: "leather",
+        color: "blue",
+        description: "Blue leather bag with butterfly chain charms with sleek strap",
+        image: "https://chany4.github.io/ecomWEB/images/leather/result%20(2).png",
+        price: 700,
+    },
+    {
+        id: 3,
+        name: "Abyss",
+        category: "leather",
+        color: "black",
+        description: "Black leather bag with white star center and sleek strap",
+        image: "https://chany4.github.io/ecomWEB/images/leather/result%20(3).png",
+        price: 600,
+    },
+    {
+        id: 4,
+        name: "Swan",
+        category: "leather",
+        color: "pink",
+        description: "Pink leather bag with silver heart sharms and sleek strap",
+        image: "https://chany4.github.io/ecomWEB/images/leather/result%20(4).png",
+        price: 1200,
+    },
+    {
+        id: 5,
+        name: "Pink Whisper",
+        category: "leather",
+        color: "pink",
+        description: "Pink leather bag with butterfly clasp and sleek strap",
+        image: "https://chany4.github.io/ecomWEB/images/leather/result%20(5).png",
+        price: 800,
+    },
+    {
+        id: 6,
+        name: "Verdant Luxe",
+        category: "leather",
+        color: "green",
+        description: "Chunky Green leather bag with thick buckle",
+        image: "https://chany4.github.io/ecomWEB/images/leather/result%20(6).png",
+        price: 1300,
+    },
+    {
+        id: 7,
+        name: "Pearl Essence",
+        category: "leather",
+        color: "white",
+        description: "Beautiful white leather bag with thin strap",
+        image: "https://chany4.github.io/ecomWEB/images/leather/result%20(7).png",
+        price: 1289,
+    },
+    {
+        id: 8,
+        name: "White Blossom",
+        category: "crochet",
+        color: "white",
+        description: "Beautiful white knitted bag with thin strap",
+        image: "https://chany4.github.io/ecomWEB/images/crochets/Crochet%20Patterns%20For%20Your%20New%20Bags%20(34%20Pics)%20(2).png",
+        price: 1289,
+    },
+    {
+        id: 9,
+        name: "Sea Foam",
+        category: "crochet",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/crochets/design%20by%204bluffscrochet.png",
+        price: 1289,
+    },
+    {
+        id: 10,
+        name: "Moss",
+        category: "crochet",
+        color: "green",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/crochets/dia.png",
+        price: 1289,
+    },
+    {
+        id: 11,
+        name: "HK Hearts",
+        category: "crochet",
+        color: "pink",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/crochets/result%20(2).png",
+        price: 1289,
+    },
+    {
+        id: 12,
+        name: "Acubi Crochet",
+        category: "crochet",
+        color: "black",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/crochets/result%20(3).png",
+        price: 1289,
+    },
+    {
+        id: 13,
+        name: "Love Knot Backpack",
+        category: "backpack",
+        color: "pink",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/backpacks/result%20(2).png",
+        price: 1289,
+    },
+    {
+        id: 14,
+        name: "BasIIk",
+        category: "backpack",
+        color: "white",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/backpacks/result%20(3).png",
+        price: 1289,
+    },
+    {
+        id: 15,
+        name: "Blue Bell",
+        category: "backpack",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/backpacks/result%20(4).png",
+        price: 1289,
+    },
+    {
+        id: 16,
+        name: "Opium",
+        category: "backpack",
+        color: "black",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/backpacks/result%20(5).png",
+        price: 1289,
+    },
+    {
+        id: 17,
+        name: "Mint",
+        category: "backpack",
+        color: "green",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/backpacks/result%20(6).png",
+        price: 1289,
+    },
+    {
+        id: 18,
+        name: "Mint",
+        category: "backpack",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/s/design%20by%204bluffs.png",
+        price: 1289,
+    },
+    {
+        id: 19,
+        name: "Mr Bones",
+        category: "teddy-Bear-Bags",
+        color: "black",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/teddyBags/Attitude%20Clothing%20Deady%20Bear%20Backpack.png",
+        price: 1289,
+    },
+    {
+        id:20,
+        name: "Cinnamanroll",
+        category: "teddy-Bear-Bags",
+        color: "white",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/teddyBags/result%20(2).png"
+    },
+    {
+        id: 21,
+        name: "Sea Foam",
+        category: "teddy-Bear-Bags",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/s/design%20by%204bluffs.png",
+        price: 1289,
+    },
+    {
+        id: 22,
+        name: "Sea Foam",
+        category: "teddy-Bear-Bags",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/s/design%20by%204bluffs.png",
+        price: 1289,
+    },
+    {
+        id: 23,
+        name: "Sea Foam",
+        category: "teddy-Bear-Bags",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/s/design%20by%204bluffs.png",
+        price: 1289,
+    },
+    {
+        id: 24,
+        name: "Sea Foam",
+        category: "teddy-Bear-Bags",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/s/design%20by%204bluffs.png",
+        price: 1289,
+    },
+    {
+        id: 25,
+        name: "Sea Foam",
+        category: "teddy-Bear-Bags",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/s/design%20by%204bluffs.png",
+        price: 1289,
+    },
+    {
+        id: 26,
+        name: "Sea Foam",
+        category: "teddy-Bear-Bags",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/s/design%20by%204bluffs.png",
+        price: 1289,
+    },
+    {
+        id: 27,
+        name: "Sea Foam",
+        category: "teddy-Bear-Bags",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/s/design%20by%204bluffs.png",
+        price: 1289,
+    },
+    {
+        id: 28,
+        name: "Sea Foam",
+        category: "teddy-Bear-Bags",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/s/design%20by%204bluffs.png",
+        price: 1289,
+    },
+    {
+        id: 29,
+        name: "Sea Foam",
+        category: "teddy-Bear-Bags",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/s/design%20by%204bluffs.png",
+        price: 1289,
+    },
+    {
+        id: 30,
+        name: "Sea Foam",
+        category: "teddy-Bear-Bags",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/s/design%20by%204bluffs.png",
+        price: 1289,
+    },
+    {
+        id: 31,
+        name: "Sea Foam",
+        category: "teddy-Bear-Bags",
+        color: "blue",
+        description: "work in progress",
+        image: "https://chany4.github.io/ecomWEB/images/s/design%20by%204bluffs.png",
+        price: 1289,
+    },
+
+    
+]
+
+
+localStorage.setItem("products", JSON.stringify(products));
+let new_products = JSON.parse(localStorage.getItem("products"));
+
+let checkedItems = JSON.parse(localStorage.getItem('checkout')) || []
+
+function add_to_cart(item){
+    checkedItems.push(item);
+    localStorage.setItem('checkout', JSON.stringify(checkedItems));
+
+    console.log('works')
+
+}
+
+function allProducts() {
+    try {
+        let container = document.querySelector('[all_products]');
+
+        new_products.forEach((item) => {
+            container.innerHTML += `
+                <div class="col-md-4">
+                    <img src="${item.image} "style= 'width:200px; height:250px'>
+                    <h2>${item.name}</h2>
+                    <p>Price: ${item.price}</p>
+                    <button class="purchase" value="${item.id}" style:"purchase:hover{background-color:green;}" onclick = 'add_to_cart(${JSON.stringify(item)})'>Add to Cart</button>     
+                </div>
+            `
+            console.log("as intended")
+        });
+    }catch{
+        console.error("error")
+    }
+}
+
+allProducts()
+
+
